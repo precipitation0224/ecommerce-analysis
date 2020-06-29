a9a2e19f27d1018707f202fd50aba5d975518fd7
Swainnnnnnn <2596021916@qq.com>
2020-06-29 13:21:19 +0800

    test

---
+++ src/main/java/net/suncaper/ecommerceanalysis/domain/UvDetailWeek.java
@@ -0,0 +1,22 @@
+package net.suncaper.ecommerceanalysis.domain;
+
+public class UvDetailWeek {
+    private int total;
+    private String wkNum;
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public String getWkNum() {
+        return wkNum;
+    }
+
+    public void setWkNum(String wkNum) {
+        this.wkNum = wkNum;
+    }
+}
